01a2bb2b800e7b6b883e6d2f13599748de963981
DemonHashira <v.logodazhki@outlook.com>
2024-06-10 22:14:26 +0300

    Optimized the product-info code

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,21 +27,29 @@ function scrollToInfo() {
 function forProductNavigationClickHandler() {
   const productNavigation = document.querySelector(selectors.productNavigation);
   const productText = document.querySelector(selectors.productText);
-  const productTextItem = productText.querySelectorAll("li");
 
-  // Get all list items in product navigation
-  const navigationItem = productNavigation.querySelectorAll("li");
-  navigationItem.forEach((navItem) => {
+  // Convert to arrays
+  const productTextItems = Array.from(productText.querySelectorAll("li"));
+  const navigationItems = Array.from(productNavigation.querySelectorAll("li"));
+
+  navigationItems.forEach((navItem) => {
     navItem.addEventListener("click", (e) => {
       const currentNavItem = e.currentTarget;
-      navigationItem.forEach((item) => item.classList.remove("active"));
-      currentNavItem.classList.add("active");
-      productTextItem.forEach((textItem) =>
+
+      // Remove "active" class from all items
+      navigationItems.forEach((item) => item.classList.remove("active"));
+      productTextItems.forEach((textItem) =>
         textItem.classList.remove("active")
       );
-      productText
-        .querySelector("li[data-id='" + currentNavItem.id + "']")
-        .classList.add("active");
+
+      // Add "active" class to clicked item
+      currentNavItem.classList.add("active");
+
+      // Find index of clicked item
+      const index = navigationItems.indexOf(currentNavItem);
+
+      // Add "active" class to corresponding product text item
+      productTextItems[index].classList.add("active");
     });
   });
 }
